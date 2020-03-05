9572a68f3503c160e2a0334d9aaa3123ce5f76b7
Hamou Maamar Sabah <saabaaah@gmail.com>
2020-03-05 20:38:36 +0100

    [+] adding templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+# La liste des articles à afficher #
+posts = [
+    {
+        'author': 'Sabah HM',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Yassine K',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # context #
+    context = {
+        'posts':posts,
+        'title':'First django website',
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
